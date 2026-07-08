3367d6e190fec1f4c89a993e9e13477eabcbe941
ojov <oticconsults@gmail.com>
2026-07-08 01:18:55 +0100

    feat: add Pageable support to TransactionRepository for improved pagination

--- src/main/java/com/ojo/cyrus/repositories/TransactionRepository.java
+++ src/main/java/com/ojo/cyrus/repositories/TransactionRepository.java
@@ -4,7 +4,7 @@ import com.ojo.cyrus.enums.MatchStatus;
 import com.ojo.cyrus.enums.TransactionStatus;
 import com.ojo.cyrus.models.entities.Transaction;
 import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
